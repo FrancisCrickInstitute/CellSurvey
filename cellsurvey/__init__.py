93f921588f04b928303b5b9c35f046e931529831
Dave Barry <david.barry@crick.ac.uk>
2026-08-02 10:57:15 +0100

    Remove unused `fig_kwargs` import from `network_analysis`
    Assisted-by: Crush:deepseek-v4-pro

--- cellsurvey/__init__.py
+++ cellsurvey/__init__.py
@@ -1,5 +1,5 @@
 from cellsurvey.utils import remove_channel_suffix, cluster_data, assign_spots_to_cells, get_colors_for_communities
 from cellsurvey.blob_detection import detect_blobs_in_tile, detect_blobs_tiled
-from cellsurvey.network_analysis import run_muspan, fig_kwargs
+from cellsurvey.network_analysis import run_muspan
 from cellsurvey.export import export_to_qupath
 from cellsurvey.cli import main
